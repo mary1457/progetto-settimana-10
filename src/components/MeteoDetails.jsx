24d4c8cd23760183e281005a73c4b30d34403499
mary1457 <piabaldoin@gmail.com>
2024-08-30 16:20:14 +0200

    ulteriori modifiche

--- src/components/MeteoDetails.jsx
+++ src/components/MeteoDetails.jsx
@@ -5,17 +5,15 @@ import { useParams } from 'react-router-dom';
 const MeteoDetails = () => {
     const [city, setCity] = useState(null);
     const [cityFour, setCityFour] = useState(null);
-    const params = useParams()
+    const params = useParams();
     const [isLoading, setIsLoading] = useState(true);
     const [isError, setIsError] = useState(false);
 
     useEffect(() => {
         fetchMeteo();
-
     }, []);
 
     const fetchMeteo = () => {
-        console.log(params)
         const API_KEY = '4581ed4a75ffef0475104ebe50dad4bb';
         const URL = `https://api.openweathermap.org/data/2.5/weather?q=${params.cityId},IT&appid=${API_KEY}&units=metric&lang=it`;
 
@@ -30,7 +28,7 @@ const MeteoDetails = () => {
             .then((data) => {
                 setCity(data);
                 setIsLoading(false);
-                fetchFour(data.coord.lon, data.coord.lat)
+                fetchFour(data.coord.lon, data.coord.lat);
             })
             .catch((error) => {
                 console.error('ERRORE!', error);
@@ -38,9 +36,7 @@ const MeteoDetails = () => {
                 setIsError(true);
             });
 
-
         const fetchFour = (lon, lat) => {
-
             const API_KEY = '4581ed4a75ffef0475104ebe50dad4bb';
             const URL = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric&lang=it`;
 
@@ -53,7 +49,8 @@ const MeteoDetails = () => {
                     }
                 })
                 .then((data) => {
-                    setCityFour(data);
+                    const groupedData = groupByTimeOfDay(data.list);
+                    setCityFour(groupedData);
                     setIsLoading(false);
                 })
                 .catch((error) => {
@@ -61,11 +58,44 @@ const MeteoDetails = () => {
                     setIsLoading(false);
                     setIsError(true);
                 });
-        }
-    }
+        };
+    };
+
+    // Funzione per raggruppare i dati per fasce orarie con un solo dato per fascia
+    const groupByTimeOfDay = (list) => {
+        return list.reduce((acc, current) => {
+            const date = current.dt_txt.split(' ')[0]; // Ottiene solo la data
+            const time = current.dt_txt.split(' ')[1]; // Ottiene solo l'ora
+            const hour = parseInt(time.split(':')[0], 10); // Ottiene l'ora in formato numerico
+
+            let period = '';
+            if (hour >= 6 && hour < 12) {
+                period = 'Mattina';
+            } else if (hour >= 12 && hour < 18) {
+                period = 'Pomeriggio';
+            } else {
+                period = 'Notte';
+            }
+
+            if (!acc[date]) {
+                acc[date] = {
+                    Mattina: null,
+                    Pomeriggio: null,
+                    Notte: null,
+                };
+            }
+
+            // Salva solo il primo dato trovato per ciascun periodo
+            if (!acc[date][period]) {
+                acc[date][period] = current;
+            }
+
+            return acc;
+        }, {});
+    };
 
     return (
-        <Container className=" h-100">
+        <Container className="h-100">
             <Row className="justify-content-center m-3">
                 <Col xs={12} sm={12} md={12} lg={6} className="text-center">
                     {isLoading ? (
@@ -77,64 +107,74 @@ const MeteoDetails = () => {
                             <Card.Body>
                                 <Card.Title>{city.name}</Card.Title>
                                 <Card.Img
-        variant="top"
-        src={`https://openweathermap.org/img/wn/${city.weather[0].icon}@2x.png`}
-      className='w-25'
-      />
+                                    variant="top"
+                                    src={`https://openweathermap.org/img/wn/${city.weather[0].icon}@2x.png`}
+                                    className="w-25"
+                                />
                                 <Card.Text>
                                     {city.weather[0].description}
                                 </Card.Text>
                                 <Card.Text>
-                                   max: {city.main.temp_max}&#176;C
+                                    max: {city.main.temp_max}&#176;C
                                 </Card.Text>
                                 <Card.Text>
-                                   min: {city.main.temp_min}&#176;C
+                                    min: {city.main.temp_min}&#176;C
                                 </Card.Text>
                                 <Card.Text>
-                                   umidità: {city.main.humidity}%
+                                    umidità: {city.main.humidity}%
                                 </Card.Text>
-
                             </Card.Body>
                         </Card>
                     ) : (
                         <p>Nessun dato meteo disponibile</p>
                     )}
                 </Col>
+            </Row>
 
-            </Row >
-            {cityFour && (<Row className="justify-content-center m-3 g-2">
-
-                {cityFour.list.map((meteo) => (
-                    <Col key={meteo.dt} xs={6} sm={6} md={4}   lg={3} xl={2}>
-                        <Card>
-                            <Card.Body>
-                            <Card.Img
-        variant="top"
-        src={`https://openweathermap.org/img/wn/${meteo.weather[0].icon}@2x.png`}
-      className='w-25'
-      />
-                                <Card.Title>{meteo.dt_txt}</Card.Title>
-                                <Card.Text>
-                                    {meteo.weather[0].description}
-                                </Card.Text>
-                                <Card.Text>
-                                    max: {meteo.main.temp_max}&#176;C
-                                </Card.Text>
-                                <Card.Text>
-                                   min: {meteo.main.temp_min}&#176;C
-                                </Card.Text>
-                                <Card.Text>
-                                    umidità: {meteo.main.humidity}%
-                                </Card.Text>
-
-                            </Card.Body>
-                        </Card>
-                    </Col>))}
-            </Row>)}
-
+            {cityFour && (
+                <Row className="justify-content-center m-3 g-2">
+                    {Object.keys(cityFour).map((date) => (
+                        <Col key={date} xs={12} >
+                            <Card>
+                                <Card.Body>
+                                    <Card.Title>{date}</Card.Title>
+                                    <Row className='d-flex'>
+                                        
+                                        {['Mattina', 'Pomeriggio', 'Notte'].map((period) => (
+                                        <Col key={period} className='text-center'>
+                                            <Card.Subtitle className="mb-2 text-muted">
+                                                {period}
+                                            </Card.Subtitle>
+                                            {cityFour[date][period] ? (
+                                                <div>
+                                                    <Card.Img
+                                                        variant="top"
+                                                        src={`https://openweathermap.org/img/wn/${cityFour[date][period].weather[0].icon}@2x.png`}
+                                                        className="w-25"
+                                                    />
+                                                    <Card.Text>
+                                                        {cityFour[date][period].weather[0].description}
+                                                    </Card.Text>
+                                                    <Card.Text>
+                                                        Temp: {cityFour[date][period].main.temp}&#176;C
+                                                    </Card.Text>
+                                                </div>
+                                            ) : (
+                                                <Card.Text>Nessun dato disponibile</Card.Text>
+                                            )}
+                                        </Col>
+                                    ))}
+                                       
+                                    </Row>
+                                   
+                                </Card.Body>
+                            </Card>
+                        </Col>
+                    ))}
+                </Row>
+            )}
         </Container>
-    )
-}
+    );
+};
 
 export default MeteoDetails;
-
